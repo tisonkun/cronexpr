28bc013e9b5e010c5377d4ca1ebec00fe5fdad4c
tison <wander4096@gmail.com>
2024-09-11 14:05:53 +0800

    feat: impl find_next_timestamp
    Signed-off-by: tison <wander4096@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,19 @@
 extern crate core;
 
 use std::collections::BTreeSet;
+use std::ops::Add;
 use std::str::FromStr;
 
 use jiff::tz::TimeZone;
-use jiff::Timestamp;
+use jiff::{Span, Timestamp, ToSpan, Unit, Zoned};
 
 mod parser;
 pub use parser::parse_crontab;
 pub use parser::ParseError;
+
+#[derive(Debug, Clone, thiserror::Error)]
+#[error("{0}")]
+pub struct Error(String);
 
 #[derive(Debug)]
 pub struct Corntab {
@@ -73,15 +78,96 @@ impl FromStr for Corntab {
 }
 
 impl Corntab {
-    pub fn find_next_timestamp_millis(&self, timestamp_millis: i64) -> Result<i64, jiff::Error> {
-        let timestamp = Timestamp::from_millisecond(timestamp_millis)?;
-        Ok(self.find_next_timestamp(timestamp).as_millisecond())
+    pub fn find_next_timestamp_millis(&self, timestamp_millis: i64) -> Result<i64, Error> {
+        Timestamp::from_millisecond(timestamp_millis)
+            .map_err(time_error_with_context("failed to parse timestamp"))
+            .and_then(|ts| self.find_next_timestamp(ts))
+            .map(|ts| ts.as_millisecond())
     }
 
-    pub fn find_next_timestamp(&self, timestamp: Timestamp) -> Timestamp {
+    pub fn find_next_timestamp(&self, timestamp: Timestamp) -> Result<Timestamp, Error> {
         let zoned = timestamp.to_zoned(self.timezone.clone());
-        dbg!(zoned).timestamp()
+
+        // checked at most 4 years to cover the leap year case
+        let bound = &zoned + 4.years();
+
+        // at least should be the next minutes
+        let mut next = zoned;
+        next = advance_time_and_round(next, 1.minute(), Some(Unit::Minute))?;
+
+        loop {
+            if next > bound {
+                return Err(Error(format!(
+                    "failed to find next timestamp in four years; end with {next}"
+                )));
+            }
+
+            match &self.months {
+                MonthExpr::Asterisk => {}
+                MonthExpr::PossibleValues(months) => {
+                    if !months.contains(&(next.month() as u8)) {
+                        let rest_days = next.days_in_month() - next.day() + 1;
+                        next = advance_time_and_round(next, rest_days.days(), Some(Unit::Day))?;
+                        continue;
+                    }
+                }
+            }
+
+            match &self.days_of_month {
+                DayOfMonthExpr::Asterisk => {}
+                DayOfMonthExpr::PossibleValues(dom) => {
+                    if !dom.contains(&(next.day() as u8)) {
+                        next = advance_time_and_round(next, 1.day(), Some(Unit::Day))?;
+                        continue;
+                    }
+                }
+            }
+
+            match &self.hours {
+                HourExpr::Asterisk => {}
+                HourExpr::PossibleValues(dom) => {
+                    if !dom.contains(&(next.hour() as u8)) {
+                        next = advance_time_and_round(next, 1.hour(), Some(Unit::Hour))?;
+                        continue;
+                    }
+                }
+            }
+
+            match &self.minutes {
+                MinuteExpr::Asterisk => {}
+                MinuteExpr::PossibleValues(dom) => {
+                    if !dom.contains(&(next.minute() as u8)) {
+                        next = advance_time_and_round(next, 1.minute(), Some(Unit::Minute))?;
+                        continue;
+                    }
+                }
+            }
+
+            break Ok(next.timestamp());
+        }
     }
+}
+
+fn advance_time_and_round(zoned: Zoned, span: Span, unit: Option<Unit>) -> Result<Zoned, Error> {
+    let mut next = zoned;
+
+    next = next
+        .checked_add(span)
+        .map_err(time_error_with_context(&format!(
+            "failed to advance timestamp; end with {next}"
+        )))?;
+
+    if let Some(unit) = unit {
+        next = next.round(unit).map_err(time_error_with_context(&format!(
+            "failed to round timestamp; end with {next}"
+        )))?;
+    }
+
+    Ok(next)
+}
+
+fn time_error_with_context(context: &str) -> impl FnOnce(jiff::Error) -> Error + '_ {
+    move |error| Error(format!("{context}: {error}"))
 }
 
 #[cfg(test)]
@@ -105,8 +191,6 @@ fn setup_logging() {
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
     use jiff::Timestamp;
 
     use crate::setup_logging;
@@ -117,8 +201,8 @@ mod tests {
         setup_logging();
 
         let crontab = "0 0 1 1 * Asia/Shanghai".parse::<Corntab>().unwrap();
-        let timestamp = Timestamp::from_str("2024-01-01T00:00:00Z").unwrap();
-        let next = crontab.find_next_timestamp(timestamp);
-        // assert_eq!(next, Timestamp::from_str("2025-01-01T00:00:00Z").unwrap());
+        let timestamp = Timestamp::now();
+        let next = crontab.find_next_timestamp(timestamp).unwrap();
+        println!("next: {}", next.to_zoned(crontab.timezone.clone()));
     }
 }
